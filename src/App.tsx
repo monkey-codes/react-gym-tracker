2d016455b981dcbbfcfbff2fdbe02035ef5830b4
monkey codes <jz@monkey.codes>
2021-09-15 15:13:56 +1000

    Reformat with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import './App.css';
-import {createTheme, CssBaseline, ThemeProvider} from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 
 function App() {
-    const theme = React.useMemo(
-        () =>
-            createTheme({
-                palette: {
-                    type: 'dark'
-                },
-            }),
-        [],
-    );
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: "dark",
+        },
+      }),
+    []
+  );
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <div className="App">
-                <header className="App-header">
-                    <h1>Hello world</h1>
-                </header>
-            </div>
-        </ThemeProvider>
-
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello world</h1>
+        </header>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
